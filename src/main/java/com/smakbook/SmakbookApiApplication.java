c22e6f3f1108c8a77ed8b166844afdc98e697557
Yuliana Bilak <bilakyuliana6996@gmail.com>
2024-11-19 12:49:04 +0200

    initial commit

---
+++ src/main/java/com/smakbook/SmakbookApiApplication.java
@@ -0,0 +1,13 @@
+package com.smakbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmakbookApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmakbookApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/smakbook/SmakbookApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smakbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmakbookApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
